5fac9c14e278cd5ccca2fc63c8b6fd366c02e871
Yeside Asekun <bola@vitalvida.ng>
2025-07-14 15:03:47 +0100

    Add simple PHP test file

--- public/test.php
+++ public/test.php
@@ -1,3 +1,8 @@
 <?php
-echo "Laravel is working!";
-phpinfo();
+echo json_encode([
+    'status' => 'PHP is working',
+    'timestamp' => date('Y-m-d H:i:s'),
+    'APP_KEY' => $_ENV['APP_KEY'] ?? 'NOT SET',
+    'env_vars' => array_keys($_ENV)
+]);
+?>
